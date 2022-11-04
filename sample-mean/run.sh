183057a429ac21e74b4da3bb58f05024349a6240
Eduardo Da Silva <alu0101104911@ull.edu.es>
2022-11-04 16:13:40 +0000

    Tocado run.sh

--- sample-mean/run.sh
+++ sample-mean/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
-
+# Cambio increible no me lo creo que increible es
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-# Load deployment environment
 . /root/.deployment.env
 
 case "$1" in
